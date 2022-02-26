de66e87a74b56550ff3debff68016bb51b340085
mhd <mhdhsna@gmail.com>
2022-02-26 23:20:57 +0300

    Dish Detail

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 
 function Contact(props) {
     return(
@@ -6,6 +8,16 @@ function Contact(props) {
             <div className="row row-content">
                 <div className="col-12">
                 <h3>Location Information</h3>
+                </div>
+                <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Contact Us</h3>
+                    <hr />
+                </div>                
                 </div>
                 <div className="col-12 col-sm-4 offset-sm-1">
                         <h5>Our Address</h5>
--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -50,16 +50,28 @@ function RenderDish({dish}) {
 
   const  DishDetail = (props) => {
     const dish = props.dish;
-    return (
-        <div className="row">
-            <div className='col-12 col-md-5 m-1'>
-                <card>
-                <RenderDish dish={dish} onClick={props.onClick} />
-                <RenderComments dish={dish}/>
-                </card>
-            </div>
-        </div>
-        )
+    return (<div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={dish} onClick={props.onClick} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        {/* <RenderComments comments={props.comments} /> */}
+                        <RenderComments dish={dish}/>
+                    </div>
+                </div>
+                </div>
+            );
     }
 
 export default DishDetail;
